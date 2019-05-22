e526d9a4c15403dd5fcf9a3a4f01a55167315a30
Santiago Moreno <santiagomoreno@santiagomoreno-macbookpro.roam.corp.google.com>
2019-05-22 17:31:00 -0500

    Progress

--- js/main.js
+++ js/main.js
@@ -4,18 +4,70 @@
 (function () {
     'use strict';
 
+    function evalScript(command) {
+        return new Promise(function(resolve, reject) {
+            csInterface.evalScript(command, resolve);
+        });
+    }
+
+    function loadJSX(fileName) {
+        var extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) + "/jsx/";
+        csInterface.evalScript('$.evalFile("' + extensionRoot + fileName + '")');
+    }
+
     var csInterface = new CSInterface();
     
     function init() {
                 
         initColors();
+
+        var text = document.getElementById('input_name')
+        var outputText = document.getElementById('output_name')
+        var inputFolder = undefined
+        var outputFolder = undefined
+
+        evalScript(`getLayers()`)
+            .then(function(res) {
+                console.log(res)
+                // return evalScript(`selectFiles()`)
+            });
                 
-        document.getElementById("btn_test").addEventListener('click', function () {
-            csInterface.evalScript('sayHello()');
-        });
+        // document.getElementById("btn_test").addEventListener('click', function () {
+        //     csInterface.evalScript('sayHello()');
+        // });
 
         document.getElementById("input_file").addEventListener('click', function () {
-            csInterface.evalScript('selectFiles()');
+            evalScript(`selectFiles("${text}")`)
+                .then(function(res) {
+                    text.innerHTML = res
+                    inputFolder = res
+                    // return evalScript(`selectFiles()`)
+                });
+                // .then(function(res)) {
+
+                // }
+        });
+
+        document.getElementById("output_file").addEventListener('click', function () {
+            evalScript(`output()`)
+                .then(function(res) {
+                    outputText.innerHTML = res
+                    outputFolder = res
+                    // return evalScript(`selectFiles()`)
+                });
+                // .then(function(res)) {
+
+                // }
+        });
+
+        document.getElementById("run").addEventListener('click', function () {
+            evalScript(`run()`)
+                .then(function(res) {
+                    // return evalScript(`selectFiles()`)
+                });
+                // .then(function(res)) {
+
+                // }
         });
     }
         
--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -1,14 +1,73 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, Folder*/
 
-
-function sayHello(){
-    alert("hello");
-}
+var files
+var doc = activeDocument;
+var allLayers = [];
 
 function selectFiles(){
-    alert('here')
-    var input = File.openDialog("Select files", getFiles, true)
+    alert(activeDocument.path)
+    var input = Folder.selectDialog("Select Folder")
+    files = input.getFiles(/\.(jpg|tif|psd|bmp|gif|png|psb|)$/i)
+    if (input != null) {
+        var inputData = {
+            name: input.name,
+            path: input.path,
+            fullName: input.fullName,
+            fsName: input.fsName,
+            relativeURI: input.relativeURI,
+            absoluteURI: input.absoluteURI
+        }
+        return inputData.absoluteURI
+    }
+}
+
+function output(){
+    var input = Folder.selectDialog("Select Output Folder")
+    // var files = input.getFiles(/\.(jpg|tif|psd|bmp|gif|png|)$/i)
+    if (input != null) {
+        var inputData = {
+            name: input.name,
+            path: input.path,
+            fullName: input.fullName,
+            fsName: input.fsName,
+            relativeURI: input.relativeURI,
+            absoluteURI: input.absoluteURI
+        }
+        return inputData.absoluteURI
+    }
+}
+
+function run(){
+    for (var i = 0; i < files.length; i++) {
+        var selected = files[i];
+        parent = replaceContents(selected, parent)
+        // Save JPG
+        activeDocument.saveAs((new File(thePath + '/Edited/' + activeDocument.name + '_' +selected.name + ".jpg")), jpgSaveOptions, true,Extension.LOWERCASE);
+        
+    }
+}
+
+function getLayers(){
+    collectAllLayers(doc, allLayers);
+    // return 'hello'
+    // collectAllLayers(theParent, level, allLayers)
+}
+
+function collectAllLayers(parent, allLayers) {
+    for (var m = 0; m < parent.layers.length; m++)
+    {
+        var currentLayer = parent.layers[m];
+        if (currentLayer.typename === "ArtLayer")
+        {
+            allLayers.push(currentLayer);
+        }
+        else {
+            collectAllLayers(currentLayer, allLayers);
+        }
+        alert(allLayers)
+    }
+    return allLayers;
 }
 
 function getFiles(input) {
